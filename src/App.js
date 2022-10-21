5e3ca1ebe0eb9f6c373c6b31a9c297fceb97f58f
Cauan <cauanbarros14@gmail.com>
2022-10-21 18:29:55 -0300

    mais um requ

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
-import solarSystem from './components/SolarSystem';
+import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        Header
-        solarSystem
+        <Header/>
+       <SolarSystem/>
       </div>
     );
   }
--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,12 +1,23 @@
-import { Component } from "react";
+import { Component } from 'react';
+import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class solarSystem extends Component {
-    render(){
-        return (
-         <div data-testid="solar-system"></div>
-        )
-        }
-    
+  render() {
+    return (
+
+      <div data-testid="solar-system">
+        <Title headline="planetas" />
+        {planets.map((planet) => (
+          <PlanetCard
+            planetName={ planet.name }
+            planetImage={ planet.image }
+          />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default solarSystem;
