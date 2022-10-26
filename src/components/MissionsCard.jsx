af537c1459822c6ce406037f6fa1abcfb252e6fc
Cauan <cauanbarros14@gmail.com>
2022-10-26 10:31:45 -0300

    agora vai

--- src/components/MissionsCard.jsx
+++ src/components/MissionsCard.jsx
@@ -1,28 +1,20 @@
-import { Component } from 'react';
-import missions from '../data/missions';
+/* import { Component } from 'react';
 
-class MissionsCard extends Component {
+class MissionCard extends Component {
   render() {
-    const { name, year, country, destination } = this.props;
+    const { name,
+      year,
+      country,
+      destination } = this.props;
     return (
       <div data-testid="mission-card">
-        <p data-testid="mission-name">{ name }</p>
-        <p data-testid="mission-year">
-          {' '}
-          { year }
-        </p>
-        <p data-testid="mission-country">
-          {' '}
-          { country }
-        </p>
-        <p data-testid="mission-destination">
-          {' '}
-          { destination }
-        </p>
+        <p data-testid="mission-name">{name}</p>
+        <p data-testid="mission-year">{year}</p>
+        <p data-testid="mission-country">{country}</p>
+        <p data-testid="mission-destination">{destination}</p>
       </div>
-
     );
   }
 }
 
-export default MissionsCard;
+export default MissionCard; */
